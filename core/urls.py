680c10f71fffb40dd965d45c98e69e994c7afa20
Hugodelima <hugorodrigues.contato@gmail.com>
2025-06-12 14:13:40 -0400

    feat: adicionado as urls da api

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from profissionais.views import ProfissionalViewSet
+from consultas.views import ConsultaViewSet
+
+router = routers.DefaultRouter()
+router.register(r'profissionais', ProfissionalViewSet)
+router.register(r'consultas', ConsultaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
+
