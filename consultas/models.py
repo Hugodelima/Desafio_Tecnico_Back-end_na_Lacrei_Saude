fb7ee8bc16463a90f663dd2c779566d808f75af8
Hugodelima <hugorodrigues.contato@gmail.com>
2025-06-12 13:53:47 -0400

    feat: criado models profissionais e consultas

--- consultas/models.py
+++ consultas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from profissionais.models import Profissional
 
-# Create your models here.
+class Consulta(models.Model):
+    data = models.DateTimeField()
+    profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE, related_name="consultas")
+
+    def __str__(self):
+        return f"Consulta com {self.profissional.nome_social} em {self.data}"
