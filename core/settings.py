179f6806fb4bbaff4cdd587a05af1b35a23b310a
Hugodelima <hugorodrigues.contato@gmail.com>
2025-06-12 19:47:18 -0400

    fix: adicionado allowed_host

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
 
 
 # Application definition
